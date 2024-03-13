4b7b3476e77c2875f6a2bd2b22fd4d6be446aea6
gfleetwood <7785756+gfleetwood@users.noreply.github.com>
2024-03-13 10:44:35 -0400

    Update create_audio_book.py

--- create_audio_book/create_audio_book.py
+++ create_audio_book/create_audio_book.py
@@ -7,44 +7,72 @@ from io import BytesIO
 from apify_client import ApifyClient
 from pdfminer.high_level import extract_text
 
-apify_client = ApifyClient(environ['APIFY_API'])
-openai_client = OpenAI(api_key = environ['OPENAI_API_KEY'])
-PDF_URL = "https://www.maths.ed.ac.uk/~v1ranick/papers/wigner.pdf"
+def extract_pdf_text_router(source, router = 'url'):
+    '''
+    router: Recognized values are either url or file
+    '''
+    
+    router_mapping = {'url': extract_pdf_text_from_url, 'file': extract_pdf_text_from_file}
+    payload = router_mapping[router](source)
+    
+    return(payload)
 
-'''
-# Use this code if the pdf is a url
-'''
+def extract_pdf_text_from_url(source):
+    '''
+    Most of this code comes from the PDF Text Extractor actor on Apify:
 
-run_input = {
-    "urls": [PDF_URL],
-    "performChunking": False,
-    "chunkSize": 1000,
-    "chunkOverlap": 0,
-}
+    https://apify.com/jirimoravcik/pdf-text-extractor
 
-run = apify_client.actor("QbKEOrw6PkLcy4Xms").call(run_input = run_input)
+    To get it from scratch, when logged in look to the right of the actor's page
+    for the API dropdown. Click that, then click Clients, and then Python.
+    '''
+    
+    apify_client = ApifyClient(environ['APIFY_API'])
+    
+    run_input = {
+        "urls": [source],
+        "performChunking": False,
+        "chunkSize": 1000,
+        "chunkOverlap": 0,
+    }
+    
+    run = apify_client.actor("QbKEOrw6PkLcy4Xms").call(run_input = run_input)
+    
+    text = '\n\n'.join([
+        item['text'] 
+        for item in apify_client.dataset(run["defaultDatasetId"]).iterate_items()
+    ])
+    
+    return(text)
 
-text = '\n\n'.join([
-    item['text'] 
-    for item in apify_client.dataset(run["defaultDatasetId"]).iterate_items()
-])
+def extract_pdf_text_from_file(source):
 
-'''
-# Use this code if it's local on your machine
+    text = extract_text(source)
+    
+    return(text)
 
-pdf_path = ''
-text = extract_text(pdf_path)
-'''
+def generate_audio(text):
 
-sentences = text.split('.')
-audio_segments = []
+    openai_client = OpenAI(api_key = environ['OPENAI_API_KEY'])
+    
+    sentences = text.split('.')
+    audio_segments = []
+    
+    for i, sentence in enumerate(sentences):
+        if len(sentence) < 1: continue
+        response = openai_client.audio.speech.create(model = "tts-1", voice = "alloy", input = sentence)
+        audio_data = BytesIO(response.content)
+        audio_segment = AudioSegment.from_file(audio_data, format = "mp3")
+        audio_segments.append(audio_segment)
+    
+    concatenated_audio = sum(audio_segments)
+    concatenated_audio.export("audio_book.mp3", format = "mp3")
+    
+    return(True)
 
-for i, sentence in enumerate(sentences):
-    if len(sentence) < 1: continue
-    response = openai_client.audio.speech.create(model = "tts-1", voice = "alloy", input = sentence)
-    audio_data = BytesIO(response.content)
-    audio_segment = AudioSegment.from_file(audio_data, format = "mp3")
-    audio_segments.append(audio_segment)
+PDF_URL_OR_FILE_PATH = "https://www.maths.ed.ac.uk/~v1ranick/papers/wigner.pdf"
+ROUTER = 'url' # or 'file' for a local file
 
-concatenated_audio = sum(audio_segments)
-concatenated_audio.export("audio_book.mp3", format = "mp3")
+text = extract_pdf_text_router(PDF_URL_OR_FILE_PATH, ROUTER)
+generate_audio(text) # The file will be saved as audio_book.mp3 in your home directory
+
